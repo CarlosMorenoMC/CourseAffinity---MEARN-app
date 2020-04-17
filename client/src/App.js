1a477ebe5024b36bed290fb0513a2bb9cf2aaae6
Carlos Moreno <carlos_moreno_mc@hotmail.com>
2020-04-17 15:45:53 +0200

    client started

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { DatePicker } from 'antd';
 
 function App() {
+
+
+  function onChange(date, dateString) {
+    console.log(date, dateString);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Course Affinity</h1>
+        <DatePicker onChange={onChange} />
       </header>
     </div>
   );
